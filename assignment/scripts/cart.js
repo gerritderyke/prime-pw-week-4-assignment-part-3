b90b19f7b6e2214f508a3b9516a20c593a4fe585
Gerrit DeRyke <gderyke@gmail.com>
2021-05-28 02:00:50 -0500

    uhhh maybe on the right track

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,17 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+ let basket = []
+
+ function addItem( item){
+
+   basket.unshift( item );
+   return basket;
+     if (item >= basket.length);{
+       return true;
+     }//end if
+    return false;
+ }//end function
+console.log(`basket is ${basket}`);
+console.log('adding kiwi (expect true)', addItem('kiwi'));
+console.log(`basket is now ${basket}`);
